b59f615b85d926ee28f6c1291a3986ab089ed313
chrisvev <christofervv97@gmail.com>
2023-11-15 12:57:03 +0100

    Añadido metodo actualizarAlumno

--- src/AlumnoCRUD.java
+++ src/AlumnoCRUD.java
@@ -73,5 +73,14 @@ public class AlumnoCRUD {
     }
 
 
+    public void bajaAlumno(String dni) {
+        ArrayList<Alumno> listaAlumno = leerAlumnos();
+        for (int i = 0; i < listaAlumno.size(); i++) {
+            if (listaAlumno.get(i).getDni().equalsIgnoreCase(dni)) {
+                listaAlumno.remove(i);
+            }
+        }
+        escribirAlumnos(listaAlumno);
+    }
 
 }
